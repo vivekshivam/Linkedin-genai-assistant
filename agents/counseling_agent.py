406beb1c74595423a22ace75e23d3702c8e5a534
Vivek Shivam Saharia <149573159+vivekshivam@users.noreply.github.com>
2025-07-06 03:16:16 +0530

    Update counseling_agent.py

--- agents/counseling_agent.py
+++ agents/counseling_agent.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-
+os.environ.get("OPENAI_API_KEY")
 llm = ChatOpenAI(model="gpt-4o", temperature=0.3)
 
 def career_counseling(jobfit_result):
